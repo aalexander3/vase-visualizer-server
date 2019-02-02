13fe6cf8cdb4b6d451789bba4d41632ac98ff80d
Arren Alexander <aralx73@gmail.com>
2019-02-02 14:40:30 -0700

    switched to yarn added seeds

--- db/seeds/seeds.js
+++ db/seeds/seeds.js
@@ -1,3 +1,20 @@
+let mongoose = require('mongoose');
+const Flower = require('../models/flower')
+const Vase = require('../models/vase')
+
+console.log('running seed file')
+// connect to the db
+mongoose.connect('mongodb://localhost/vase-visualizer', { useNewUrlParser: true })
+// Delete all flowers and vases prior to seeding!
+Flower.deleteMany(()=> console.log('deleted flower'))
+Vase.deleteMany(()=> console.log('deleted vases'))
+
+Flower.create([{ name: "Tulip" },{ name: "Crocus" }, { name: "Orchid" }, { name: "Snowdrop" }, { name: "Rose" } ])
+  .finally(() => {
+    console.log('bye')
+    mongoose.connection.close();
+  })
+// disconnect
 
 
-const tulip = new Flower
+//end
